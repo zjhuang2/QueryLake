bd80b8a2f7c33ee8f407d933cbc90444ba494354
zjhuang2 <jeremy.zq.huang@gmail.com>
2022-07-13 13:38:59 -0400

    Add Excluded Terms container and Topical Trend term section

--- src/theme.jsx
+++ src/theme.jsx
@@ -2,8 +2,8 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   fonts: {
-    heading: `'Manrope', sans-serif`,
-    body: `'Manrope', sans-serif`,
+    heading: `'Open Sans', sans-serif`,
+    body: `'Open Sans', sans-serif`,
   },
 });
 
