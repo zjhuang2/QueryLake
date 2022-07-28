608db211706dcaad6a34e63f00f3d3b4228fc7ab
zjhuang2 <jeremy.zq.huang@gmail.com>
2022-07-28 19:22:41 -0400

    Add Lib to GitIgnore

--- frontend/src/lib/firestoreCollection.js
+++
@@ -1,9 +0,0 @@
-import { collection } from "firebase/firestore";
-import { db } from "./firebase-config";
-
-export const mentalPerformanceCollectionRef = collection(
-  db,
-  "mentalperformance"
-);
-
-export const immunityRef = collection(db, "immunity");
